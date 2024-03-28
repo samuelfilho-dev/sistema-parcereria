2e8ef71220717ccf9981ed8429a8ad2f37d179a8
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-28 17:33:15 -0300

    refactor: spaces of function

--- models/models.js
+++ models/models.js
@@ -1,29 +1,22 @@
 export function Autor(nome, sobrenome, email, senha) {
-  const id = Math.floor(Math.random() * 1_000) + 1;
+    const id = Math.floor(Math.random() * 1_000) + 1;
 
-  this.id = id;
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.email = email;
-  this.senha = senha;
+    this.id = id;
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.email = email;
+    this.senha = senha;
 }
 
-export function ParecerJuridico(
-  assunto,
-  ementa,
-  justificacao,
-  conclusao,
-  notaDeClassificacao,
-  autor
-) {
-  const id = Math.floor(Math.random() * 1_000) + 1;
-  const titulo = `PARECER JURIDICO Nº ${id}/2024`;
+export function ParecerJuridico(assunto, ementa, justificacao, conclusao, notaDeClassificacao, autor) {
+    const id = Math.floor(Math.random() * 1_000) + 1;
+    const titulo = `PARECER JURIDICO Nº ${id}/2024`;
 
-  this.titulo = titulo;
-  this.assunto = assunto;
-  this.ementa = ementa;
-  this.justificacao = justificacao;
-  this.conclusao = conclusao;
-  this.notaDeClassificacao = notaDeClassificacao;
-  this.autor = autor;
+    this.titulo = titulo;
+    this.assunto = assunto;
+    this.ementa = ementa;
+    this.justificacao = justificacao;
+    this.conclusao = conclusao;
+    this.notaDeClassificacao = notaDeClassificacao;
+    this.autor = autor;
 }
