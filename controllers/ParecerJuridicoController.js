9a320168cc2eae1fb268c53f94fc771d43b63840
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-03-28 17:51:16 -0300

    feat: create CRUD of Adivice

--- controllers/ParecerJuridicoController.js
+++ controllers/ParecerJuridicoController.js
@@ -5,7 +5,7 @@ import {ParecerJuridico} from "../models/models.js";
 let pareceresJuricios = [];
 let autores = [];
 
-function criarParcerJuridico(assunto, ementa, justificacao, conclusao) {
+function criarParcerJuridico(assunto, ementa, justificacao, conclusao, autor) {
     return new ParecerJuridico(
         assunto,
         ementa,
@@ -20,41 +20,48 @@ function lerDados() {
     const data = fs.readFileSync('../mock.json', {encoding: "utf-8", flag: "r"});
     const jsonData = JSON.parse(data);
 
-    jsonData['pareceres'].forEach((parecer) => {
-        pareceresJuricios.push(parecer);
-    });
-
     jsonData['autores'].forEach((autor) => {
         autores.push(autor);
     });
+
+    jsonData['pareceres'].forEach((parecer) => {
+        const dbParecer = new criarParcerJuridico(parecer.assunto, parecer.ementa, parecer.justificacao, parecer.conclusao, autores[0]);
+        pareceresJuricios.push(dbParecer);
+    });
+
 }
 
-function atualizarParecerJuridico(id) {
-    // TODO: Buscar Parecer Juridico Pelo Id
-    // TODO: Atualizar Parecer encontrado pelo Id
+function atualizarParecerJuridico(id, assunto, ementa, justifacao, conclusao) {
+    const parecerJuridico = encontrarParecerJuridicoPeloId(id);
+
+    parecerJuridico.assunto = assunto;
+    parecerJuridico.ementa = ementa;
+    parecerJuridico.justificacao = justifacao;
+    parecerJuridico.conclusao = conclusao;
+
+    return parecerJuridico;
 }
 
 function deletarParecerJuridico(id) {
-    // TODO: Buscar Parecer Juridico Pelo Id
-    // TODO: Deletar Parecer encontrado pelo Id
+    const index = pareceresJuricios.findIndex(parecer => parecer.id === id)
+    pareceresJuricios.splice(index, 1);
 }
 
 function encontrarParecerJuridicoPeloId(id) {
-    // TODO: Buscar Parecer Juridico Pelo Id
-    // TODO: Entregar Parecer encontrado pelo Id
+    return pareceresJuricios.find(parecer => parecer.id = id);
 }
 
 function encontrarParecerJuridicoPeloNome(nome) {
-    // TODO: Buscar Parecer Juridico Pelo Nome
-    // TODO: Entregar Parecer encontrado pelo Nome
+    return pareceresJuricios.find(parecer => parecer.titulo = nome);
 }
 
 function encontrarParecerJuridicos() {
     return pareceresJuricios;
 }
 
-function avaliarParecerJuridico(nota) {
+function avaliarParecerJuridico(id, nota) {
+    const parecerJuridico = encontrarParecerJuridicoPeloId(id);
+    parecerJuridico.notaDeClassificacao.push(nota);
 }
 
 lerDados();
-
